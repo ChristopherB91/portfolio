bb9ce27546b1eeee2abf697421bde419209e39c3
Chris <chrisbrowne91@gmail.com>
2024-12-13 06:31:00 -0500

    finished and styled future component

---
+++ src/components/future.tsx
@@ -0,0 +1,20 @@
+export const Future = () => {
+  return (
+    <div id="future">
+      <h2>What is in my future?</h2>
+      <h3>Gauranteed</h3>
+      <ul>
+        <li>New and improved version of my prison reform project</li>
+        <li>More data structure practice</li>
+        <li>E-commerce</li>
+        <li>Deeper understanding of java</li>
+        <li>Learning oracle</li>
+        <li>Learning ML</li>
+        <b>
+          <h3>Possible</h3>
+        </b>
+        <li>3js</li>
+      </ul>
+    </div>
+  );
+};
