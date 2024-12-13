ac0b301e14ef575ee634822054e53c46d618033b
Chris <chrisbrowne91@gmail.com>
2024-12-13 05:14:18 -0500

    finished styling

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,6 +1,7 @@
 import PR from "../assets/projectPictures/PRWebsite.png";
 import omni from "../assets/projectPictures/omniDex.jpeg";
 import fig from "../assets/projectPictures/figmaToDevelopment.jpeg";
+import { useState } from "react";
 
 interface Project {
   title: string;
@@ -12,12 +13,12 @@ interface Project {
 
 const projects: Project[] = [
   {
-    title: "prison Reform Job board",
+    title: "Prison Reform Job board",
     description:
       "2/3 ex-felons end up going back to jail. My idea was to give them a chance to try again and be better. Sort of like linkedin but for reformed prisoners.",
     image: `${PR}`,
     link: "https://christopherb91.github.io/pr/",
-    skills: ["HTML", "CSS", "JS", "React"],
+    skills: ["HTML,", "CSS,", "JS,", "React"],
   },
   {
     title: "FigProject1",
@@ -25,36 +26,62 @@ const projects: Project[] = [
       "I built a website based on a random design I found on figma. The actual design it is based on can be found at the bottom of the site. ",
     image: `${fig}`,
     link: "https://christopherb91.github.io/FigProject1/",
-    skills: ["HTML", "CSS", "JS", "TS", "React", "Figma"],
+    skills: ["HTML,", "CSS,", "JS,", "TS,", "React,", "Figma"],
   },
   {
-    title: "omniDex",
+    title: "OmniDex",
     description:
       "I took the idea of the pokedex a basic front-end developer project and changed it into an intermediate full-stack application. The omnidex uses alien characters from the show Ben 10 and then displays their specie name, nickname, and skillset.",
     image: `${omni}`,
     link: "https://omnidex.vercel.app",
     skills: [
-      "HTML",
-      "CSS",
-      "Tailwind",
-      "JS",
-      "TS",
-      "React",
-      "Java",
+      "HTML,",
+      "CSS,",
+      "Tailwind,",
+      "JS,",
+      "TS,",
+      "React,",
+      "Java,",
       "Spring-boot",
     ],
   },
 ];
 
 export const Projects = () => {
+  const [num, setNum] = useState<number>(0);
+  const postChange = (wrd: string) => {
+    if (wrd === "left") {
+      if (num === 0) {
+        setNum(projects.length - 1);
+      } else {
+        setNum(num - 1);
+      }
+    } else if (wrd === "right") {
+      if (num === projects.length - 1) {
+        setNum(0);
+      } else {
+        setNum(num + 1);
+      }
+    }
+  };
+
   return (
-    <div>
-      <h2>{projects[0].title}</h2>
-      <p>{projects[0].description}</p>
-      <img src={projects[0].image} alt="website image" />
+    <div id="projects">
+      <h2>{projects[num].title}</h2>
+      <p>{projects[num].description}</p>
+      <div>
+        <input type="button" onClick={() => postChange("left")} value="<-" />
+        <a href={projects[num].link} target="_blank">
+          <img src={projects[num].image} alt="website image" />
+        </a>
+        <input type="button" onClick={() => postChange("right")} value="->" />
+      </div>
       <ul>
-        {projects[0].skills.map((skill, index) => (
-          <li key={index}>{skill}</li>
+        <p>Skills used in the project:</p>
+        {projects[num].skills.map((skill, index) => (
+          <li key={index}>
+            <b>{skill}</b>
+          </li>
         ))}
       </ul>
     </div>
